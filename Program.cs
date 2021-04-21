9a2b309650f26ca63c4a27af0b4fa6fd56d5ed05
Mary Toth <maryarylynn@gmail.com>
2021-04-21 14:49:34 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using JurasicPark.Models;
+using System.Linq;
+
+namespace JurasicPark
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to C#");
+
+    }
+  }
+}
