6e598725773b3c7f64bff8e6922283ea27742ea3
Mary Toth <maryarylynn@gmail.com>
2021-04-21 22:31:02 -0400

    finished I think

--- Program.cs
+++ Program.cs
@@ -18,10 +18,7 @@ namespace JurasicPark
             return ($"The dinosaur's name is {Name}. Their diet type is {DietType} and their weight is {Weight} pounds. They were acquired {WhenAcquired}. You can find this dino in enclosure number {EnclosureNumber}.");
         }
     }
-    // class DinosaurDatabase
-    // {
-    //     //not sure what goes here
-    // }
+
     class Program
     {
         static void WelcomeMessage()
@@ -50,7 +47,8 @@ namespace JurasicPark
 
             //Make a list of Dinosaurs-
             var dinosaurs = new List<Dinosaur>()
-             {   //not sure if I was supposed to do this part or not:
+
+             {   //not sure if I was supposed to do this part or not? 
                  new Dinosaur(){Name = "Swulf", DietType = "carnivore", Weight = 65000, EnclosureNumber = 42},
                  new Dinosaur(){Name = "Yarbo", DietType = "herbivore", Weight = 48000, EnclosureNumber = 32},
                  new Dinosaur(){Name = "Dina", DietType = "herbivore", Weight = 72000, EnclosureNumber = 22},
@@ -58,13 +56,11 @@ namespace JurasicPark
 
              };
 
-            // var database = new DinosaurDatabase();
-
             var keepGoing = true;
 
             while (keepGoing)
             {
-                Console.WriteLine("Do you want to View, Summarize, Add, Remove, Transfer, or Quit? ");
+                Console.WriteLine("Do you want to View, Summarize, Add, Remove, Transfer, Description, or Quit? ");
                 var choice = Console.ReadLine().ToLower();
 
                 //QUIT - this will stop the program
@@ -78,7 +74,7 @@ namespace JurasicPark
                     var foundHerb = dinosaurs.Count(dinosaur => dinosaur.DietType == "herbivore");
                     var foundCarn = dinosaurs.Count(dinosaur => dinosaur.DietType == "carnivore");
 
-                    Console.WriteLine($"There are {foundCarn} carnivores and {foundHerb} herbivores in the dino zoo.");
+                    Console.WriteLine($"There are {foundCarn} carnivores and {foundHerb} herbivores in the park.");
                 }
                 //VIEW - show all the dinosaurs in the list, ordered by WhenAcquired
                 // if there aren't any dinosaurs in the park then print out a message that there aren't any.
@@ -123,17 +119,29 @@ namespace JurasicPark
 
                     Console.WriteLine($"You have deleted {removeName} from the database.");
                 }
+
+                //description - this gives all the info on all the dinos 
+                else if (choice == "description")
+                {
+                    if (dinosaurs.Count > 0)
+                    {
+                        foreach (var dinosaur in dinosaurs)
+                            Console.WriteLine(dinosaur.Description());
+                    }
+                }
+
                 // TRANSFER will prompt the user for a dinosaur name and a new EnclosureNumber and update that dino's information.
+
                 else
                 {
 
-                    Console.WriteLine("Which dino do you want to update? ");
+                    Console.Write("Which dino do you want to update? ");
 
                     var dinoToUpdate = Console.ReadLine();
 
                     Dinosaur dinoUpdate = dinosaurs.First(dinosaur => dinosaur.Name == dinoToUpdate);
 
-                    Console.WriteLine("What is their new enclosure number?");
+                    Console.Write("What is their new enclosure number? ");
 
                     var newNumber = int.Parse(Console.ReadLine());
 
